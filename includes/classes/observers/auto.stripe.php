4bfe1aa35f7e42e4219774f50621747b2c0d88ec
lat9 <cindy@vinosdefrutastropicales.com>
2025-11-03 12:43:09 -0500

    Correcting errors
    Responsive classic with OPC and not

--- includes/classes/observers/auto.stripe.php
+++ includes/classes/observers/auto.stripe.php
@@ -1,16 +1,19 @@
 <?php
 class zcObserverStripe
 {
-    if (defined('MODULE_PAYMENT_STRIPE_STATUS') && MODULE_PAYMENT_STRIPE_STATUS === 'True') {
-        if (defined('MODULE_PAYMENT_STRIPE_TEST_MODE') && MODULE_PAYMENT_STRIPE_TEST_MODE === 'True') {
-            global $messageStack;
-            $messageStack->add('header', 'STRIPE IS IN TESTING MODE', 'warning');
-        }
-        
-        global $current_page_base, $db;
-        if ($current_page_base === FILENAME_ACCOUNT && isset($_POST['Delete'])) {
-            $db->Execute("DELETE FROM " . TABLE_STRIPE . " WHERE customers_id = " . (int)$_SESSION['customer_id']);
-            zen_redirect(zen_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+    public function __construct()
+    {
+        if (defined('MODULE_PAYMENT_STRIPE_STATUS') && MODULE_PAYMENT_STRIPE_STATUS === 'True') {
+            if (defined('MODULE_PAYMENT_STRIPE_TEST_MODE') && MODULE_PAYMENT_STRIPE_TEST_MODE === 'True') {
+                global $messageStack;
+                $messageStack->add('header', 'STRIPE IS IN TESTING MODE', 'warning');
+            }
+            
+            global $current_page_base, $db;
+            if ($current_page_base === FILENAME_ACCOUNT && isset($_POST['Delete'])) {
+                $db->Execute("DELETE FROM " . TABLE_STRIPE . " WHERE customers_id = " . (int)$_SESSION['customer_id']);
+                zen_redirect(zen_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+            }
         }
     }
 }
--- includes/modules/pages/checkout_one_confirmation/jscript_stripe_checkout_one_confirmation.php
+++ includes/modules/pages/checkout_one_confirmation/jscript_stripe_checkout_one_confirmation.php
@@ -2,18 +2,18 @@
 if (defined('MODULE_PAYMENT_STRIPE_STATUS') && MODULE_PAYMENT_STRIPE_STATUS === 'True' && $stripe_select === 'True') {
 ?>
 <script src="https://js.stripe.com/v3/"></script>
-<script src="includes/checkout.js" defer></script>
 <script id="stripe-form">
 $(document).ready(function(){
     let stripeForm = $('<form>', {'id': 'payment-form'});
     stripeForm.append('<div id="payment-head" style="color: #2254dd;  font-size: 24px;  font-weight: bold; margin:24px 0 12px;">Stripe</div>');
     stripeForm.append('<div id="payment-element"><!--Stripe.js injects the Payment Element--></div>');
-    stripeForm.append('<h2 style="color: #2254dd;  font-size: 24px;  font-weight: bold;">Stripe</h2>');
     stripeForm.append('<div id="payment-message" class="hidden"></div>');
     stripeForm.append('<button id="submit"><div class="spinner hidden" id="spinner"></div><span id="button-text"><?= BUTTON_CONFIRM_ORDER_ALT ?></span></button>');
     stripeForm.append('<div id="payment-foot"><?= TEXT_PAYMENT_STRIPE_MODULE_CORP;?></div>');
     
     $('#checkout_confirmation').before(stripeForm);
+
+    <?= file_get_contents('includes/checkout.js') ?>
 });
 </script>
 <?php
